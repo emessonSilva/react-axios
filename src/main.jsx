32e8a52728f51791b178092330e2d2e9b4cef2e3
Emesson <emessonbarbosa600@gmail.com>
2023-12-28 00:28:36 -0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
+
+import Home from './routes/Home.jsx';
+import NewPost from './routes/NewPost.jsx';
+
+import './index.css'
+
+const router = createBrowserRouter([
+  {
+    element: <App>
+
+    </App>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+      {
+        path: "/new",
+        element: <NewPost></NewPost>
+      }
+    ]
+    
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router = {router}></RouterProvider>
+  </React.StrictMode>,
+)
---
+++ src/routes/NewPost.jsx
@@ -0,0 +1,43 @@
+import blogFetch from '../axios/config';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import './NewPost.css';
+
+const NewPost = () => {
+  const navigate = useNavigate()
+
+  const [title, setTitle] = useState()
+  const [body, setBody] = useState()
+
+  const createPost = async (e) => {
+    e.preventDefault();
+
+    const post = {title, body, userId: 1};
+
+    await blogFetch.post("/posts", {
+      body: post,
+    });
+
+    navigate("/");
+  };
+
+  return (
+    <div className='new-post'>
+      <h2>Insert new post:</h2>
+      <form onSubmit={(e) => createPost(e)}>
+        <div className='form-control'>
+          <label htmlFor='title'>Title:</label>
+          <input type='text' name='title' id='title' placeholder='Type the title' onChange={(e) => setTitle(e.target.value)}></input>
+        </div>
+        <div className='form-control'>
+          <label htmlFor='Body'>Content:</label>
+          <textarea name='body' id='body' placeholder='Type the content'  onChange={(e) => setBody(e.target.value)}></textarea>
+        </div>
+        <input type='submit' value="create post" className='btn'></input>
+      </form>
+    </div>
+  )
+};
+
+export default NewPost
